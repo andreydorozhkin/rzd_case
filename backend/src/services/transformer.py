d3bbc8f1a7aaef7263fd2cd80e714f8134f0b3b3
tikey <ilya.hvor@gmail.com>
2023-09-09 12:29:52 +0300

    Add transform

--- backend/src/services/transformer.py
+++ backend/src/services/transformer.py
@@ -1,3 +1,4 @@
+from src.dtos import TransformedDataDTO, PlatformDTO, CargoDTO
 from src.forms import CreatePlatformForm
 
 
@@ -5,5 +6,6 @@ class TransformerService:
     def __init__(self):
         pass
 
-    def execute(self, form: CreatePlatformForm) -> ...:
-        ...
+    def execute(self, form: CreatePlatformForm) -> TransformedDataDTO:
+        return TransformedDataDTO(platform=PlatformDTO(**form.platform.model_dump()),
+                                  cargos=[CargoDTO(**c.model_dump()) for c in form.cargo_list])

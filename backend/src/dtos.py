b11eac8844ffab90f56355876cdb29ed6c58dccc
tikey <ilya.hvor@gmail.com>
2023-09-09 12:29:32 +0300

    Add dtos

---
+++ backend/src/dtos.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+
+
+class PlatformDTO(BaseModel):
+    length: int
+    width: int
+    mass: int
+    height: int
+    center_of_gravity_height: int
+    platform_base: int
+    wheel_radius: int
+
+
+class CargoDTO(BaseModel):
+    name: str
+    length: int
+    width: int
+    height: int
+    count: int
+    weight: int
+
+
+class CoordinateDTO(BaseModel):
+    x: int
+    y: int
+
+
+class PositionedCargoDTO(BaseModel):
+    cargo: CargoDTO
+    coordinate: CoordinateDTO
+
+
+class TransformedDataDTO(BaseModel):
+    platform: PlatformDTO
+    cargos: list[CargoDTO]
+
+
+class CalculatedDataDTO(BaseModel):
+    platform: PlatformDTO
+    positioned_cargos: list[PositionedCargoDTO]

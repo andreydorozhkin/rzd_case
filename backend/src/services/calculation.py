bbad6d85178f546123dec2b2c6fe19ed13f31e6a
tikey <ilya.hvor@gmail.com>
2023-09-09 12:30:02 +0300

    Use dtos typings in services

--- backend/src/services/calculation.py
+++ backend/src/services/calculation.py
@@ -1,9 +1,9 @@
-from src.forms import CreatePlatformForm
+from src.dtos import TransformedDataDTO, CalculatedDataDTO
 
 
 class CalculationService:
     def __init__(self):
         pass
 
-    def execute(self, data) -> ...:
+    def execute(self, data: TransformedDataDTO) -> CalculatedDataDTO:
         ...
--- backend/src/services/generation.py
+++ backend/src/services/generation.py
@@ -1,6 +1,9 @@
+from src.dtos import CalculatedDataDTO
+
+
 class GenerationService:
     def __init__(self):
         pass
 
-    def execute(self, data) -> str:
+    def execute(self, data: CalculatedDataDTO) -> str:
         return "files/test.txt"
